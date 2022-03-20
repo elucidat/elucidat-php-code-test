e0809ac6e37d7ccac1743085b2b55098d29926f0
Ian S <ies20@sussex.ac.uk>
2022-03-20 08:57:17 +0000

    Moved the rest of the logic for the item types into separate functions which now passes all tests with original nextDay logic commented out

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -24,50 +24,56 @@ class GildedRose
         foreach ($this->items as $item) {
             if($item->name === 'normal'){
                 $this->normalNextDay($item);
-            } else {
-                if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->quality > 0) {
-                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality = $item->quality - 1;
-                        }
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                        if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                            if ($item->sellIn < 11) {
-                                if ($item->quality < 50) {
-                                    $item->quality = $item->quality + 1;
-                                }
-                            }
-                            if ($item->sellIn < 6) {
-                                if ($item->quality < 50) {
-                                    $item->quality = $item->quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                    $item->sellIn = $item->sellIn - 1;
-                }
-                if ($item->sellIn < 0) {
-                    if ($item->name != 'Aged Brie') {
-                        if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                            if ($item->quality > 0) {
-                                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                    $item->quality = $item->quality - 1;
-                                }
-                            }
-                        } else {
-                            $item->quality = $item->quality - $item->quality;
-                        }
-                    } else {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                }
+            } elseif($item->name === 'Aged Brie') {
+                $this->brieNextDay($item);
+            } elseif($item->name === 'Sulfuras, Hand of Ragnaros') {
+                $this->sulfurasNextDay($item);
+            }elseif($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
+                $this->passesNextDay($item);
+            }else{
+//                if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+//                    if ($item->quality > 0) {
+//                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+//                            $item->quality = $item->quality - 1;
+//                        }
+//                    }
+//                } else {
+//                    if ($item->quality < 50) {
+//                        $item->quality = $item->quality + 1;
+//                        if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
+//                            if ($item->sellIn < 11) {
+//                                if ($item->quality < 50) {
+//                                    $item->quality = $item->quality + 1;
+//                                }
+//                            }
+//                            if ($item->sellIn < 6) {
+//                                if ($item->quality < 50) {
+//                                    $item->quality = $item->quality + 1;
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+//                    $item->sellIn = $item->sellIn - 1;
+//                }
+//                if ($item->sellIn < 0) {
+//                    if ($item->name != 'Aged Brie') {
+//                        if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
+//                            if ($item->quality > 0) {
+//                                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+//                                    $item->quality = $item->quality - 1;
+//                                }
+//                            }
+//                        } else {
+//                            $item->quality = $item->quality - $item->quality;
+//                        }
+//                    } else {
+//                        if ($item->quality < 50) {
+//                            $item->quality = $item->quality + 1;
+//                        }
+//                    }
+//                }
             }
         }
     }
@@ -80,6 +86,46 @@ class GildedRose
 
         if ($item->quality <= 0) {
             $item->quality = 0;
+        }
+
+        $item->sellIn -= 1;
+    }
+
+    public function brieNextDay($item)
+    {
+        $item->quality += 1;
+        if ($item->sellIn <= 0) {
+            $item->quality += 1;
+        }
+
+        if ($item->quality > 50) {
+            $item->quality = 50;
+        }
+
+        $item->sellIn -= 1;
+    }
+
+    public function sulfurasNextDay($item)
+    {
+        // Does nothing
+    }
+
+    public function passesNextDay($item)
+    {
+        $item->quality += 1;
+        if ($item->sellIn <= 10) {
+            $item->quality += 1;
+        }
+        if ($item->sellIn <= 5) {
+            $item->quality += 1;
+        }
+
+        if ($item->sellIn <= 0) {
+            $item->quality = 0;
+        }
+
+        if ($item->quality > 50) {
+            $item->quality = 50;
         }
 
         $item->sellIn -= 1;
