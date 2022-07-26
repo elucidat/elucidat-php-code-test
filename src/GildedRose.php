2f089ff2410404c1e3a7171c29c47077a57aa48d
ChangKhao <mark.berrecloth@gmail.com>
2022-07-27 00:15:43 +0100

    refactored GildedRose class

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -22,57 +22,58 @@ class GildedRose
     public function nextDay()
     {
         foreach ($this->items as $item) {
-            
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        if ($item->name == 'Conjured Mana Cake' and $item->quality > 1) {
-                            $item->quality = $item->quality - 2;
-                        } else {
-                          $item->quality = $item->quality - 1;  
-                        }
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
+            switch($item->name) {
+                case 'normal' : $this->normal($item); break;
+                case 'Aged Brie' : $this->agedBrie($item); break;
+                case 'Sulfuras, Hand of Ragnaros' : $this->sulfuras($item); break;
+                case 'Backstage passes to a TAFKAL80ETC concert' : $this->passes($item);  break;
+                case 'Conjured Mana Cake' : $this->conjured($item); break;
             }
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                if ($item->name == 'Conjured Mana Cake' and $item->quality > 1) {
-                                    $item->quality = $item->quality - 2;
-                                } else {
-                                  $item->quality = $item->quality - 1;  
-                                }
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+        }
+    }
+
+    public function normal($item) {
+        if ($item->quality > 0) { $item->quality -= 1; }
+        $item->sellIn -= 1;
+        if ($item->sellIn < 0) {
+            if ($item->quality > 0) { $item->quality -= 1; }
+        }
+    }
+
+    public function agedBrie($item) {
+        if ($item->quality < 50) { $item->quality += 1; }
+        $item->sellIn -= 1;
+        if ($item->sellIn < 0) {
+            if ($item->quality < 50) { $item->quality += 1; }
+        }
+    }
+
+    public function sulfuras($item) {
+        return true;
+    }
+
+    public function passes($item) {
+        if ($item->quality < 50) { 
+            $item->quality += 1; 
+            if ($item->sellIn < 11) { $item->quality += 1; }
+            if ($item->sellIn < 6) { $item->quality += 1; }
+        }
+        $item->sellIn -= 1;
+        if($item->sellIn < 0) {
+            $item->quality = 0;
+        }
+    }
+
+    public function conjured($item) {
+        switch(true){
+            case ($item->quality > 1) : $item->quality -= 2; break;
+            case ($item->quality < 2) : $item->quality = 0;
+        }
+        $item->sellIn -= 1;
+        if ($item->sellIn < 0) {
+            switch(true){
+                case ($item->quality > 1) : $item->quality -= 2; break;
+                case ($item->quality < 2) : $item->quality = 0;
             }
         }
     }
